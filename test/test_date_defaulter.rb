8db847de5365443e4bd8f1622b5590c0a4638fca
Shkumbin Delija <shkumbin.delija@gmail.com>
2023-02-23 14:29:41 +0100

    Fix tests

--- test/test_date_defaulter.rb
+++ test/test_date_defaulter.rb
@@ -3,14 +3,46 @@ require_relative '../lib/date_defaulter'
 
 class DateDefaulterTest < Minitest::Test
   def test_to_s
-    DateDefaulter.configuration.format = '%d/%m/%Y'
+    date = Date.new(2016, 10, 21)
 
-    assert_equal(Date.new(2022).to_s, '01/01/2022')
+    DateDefaulter.format = '%d/%m/%Y'
+    assert_equal(date.to_s, '21/10/2016')
+
+    DateDefaulter.format = '%m/%d/%Y'
+    assert_equal(date.to_s, '10/21/2016')
+
+    DateDefaulter.format = '%Y/%m/%d'
+    assert_equal(date.to_s, '2016/10/21')
   end
 
-  def test_stftime
-    DateDefaulter.configuration.format = '%d/%m/%Y'
+  def test_strftime
+    date = Date.new(2016, 10, 21)
 
-    assert_equal(Date.new(2022).strftime, '01/01/2022')
+    DateDefaulter.format = '%d/%m/%Y'
+    assert_equal(date.strftime, '21/10/2016')
+
+    DateDefaulter.format = '%m/%d/%Y'
+    assert_equal(date.strftime, '10/21/2016')
+
+    DateDefaulter.format = '%Y/%m/%d'
+    assert_equal(date.strftime, '2016/10/21')
+  end
+
+  def test_format=
+    DateDefaulter.format = 'test'
+
+    assert_equal(DateDefaulter.format, 'test')
+  end
+
+  def test_configuration
+    assert_equal(DateDefaulter.configuration, DateDefaulter::Configuration.instance)
+  end
+
+  def test_setting_format_with_configuration
+    DateDefaulter.configuration do |configuration|
+      configuration.format = 'hello'
+    end
+
+    assert_equal(DateDefaulter.format, 'hello')
   end
 end
