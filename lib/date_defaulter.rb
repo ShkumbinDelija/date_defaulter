71dd5ecb8c708980f192824267e5e6dbb00babcf
Shkumbin Delija <shkumbin.delija@gmail.com>
2023-02-23 13:58:43 +0100

    Overwrite to_s and strftime

--- lib/date_defaulter.rb
+++ lib/date_defaulter.rb
@@ -1,7 +1,46 @@
 require 'date'
 
+module DateDefaulter
+  @@config = {}
+
+  class Configuration
+    @@instance = Configuration.new
+
+    attr_accessor :format
+
+    private_class_method :new
+
+    def self.instance
+      @@instance
+    end
+  end
+
+  def self.configuration
+    Configuration.instance.tap do |configuration|
+      yield configuration if block_given?
+    end
+  end
+end
+
 class Date
+  alias_method :date_defaulter_strftime, :strftime
+  alias_method :date_defaulter_to_s, :to_s
+
   def to_s
-    strftime('%d/%m/%Y')
+    if DateDefaulter.configuration.format.nil?
+      strftime(DateDefaulter.configuration.format)
+    else
+      date_defaulter_to_s
+    end
+  end
+
+  def strftime(*args)
+    return date_defaulter_strftime(*args) if args.any?
+
+    if DateDefaulter.configuration.format.nil?
+      date_defaulter_strftime
+    else
+      date_defaulter_strftime(DateDefaulter.configuration.format)
+    end
   end
 end
